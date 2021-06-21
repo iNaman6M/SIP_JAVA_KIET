f909dd28a2caddc19362346622e660e193453cb0
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-06-22 00:57:57 +0530

    Day 1 basic-gfg

---
+++ src/ScannerInput.java
@@ -0,0 +1,16 @@
+//22 june 2021
+//Program for taking input from user
+//Basic - gfg
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class ScannerInput {
+	public static void main(String []args) {
+		
+			Scanner sc =new Scanner(System.in);
+			System.out.println("Enter the number");
+			int n = sc.nextInt();
+			System.out.println("THe number is : "+n);
+	}
+}
